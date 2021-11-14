a2df8867892ecd135645f60a30dca7f1dfe0f0f3
MarSegu <mariosegura0904@gmail.com>
2021-11-14 18:17:00 -0500

    Formulario de personas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,17 @@ import { Persona } from './persona.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Listado de Personas';
-  personas: Persona[] = [new Persona('Juan', 'Perez'), 
+	titulo = 'Listado de Personas';
+	personas: Persona[] = [new Persona('Juan', 'Perez'), 
   						new Persona('Laura', 'Juarez'),
 						new Persona('Carla', 'Gomez')];
+	nombreInput: string = '';
+	apellidoInput: string = '';
+
+	agregarPersona(): void{
+		let persona1 = new Persona( this.nombreInput, this.apellidoInput);
+		this.personas.push(persona1);
+		this.nombreInput = '';
+		this.apellidoInput = '';
+	}
 }
